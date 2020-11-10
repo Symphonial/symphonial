902123be6f9a07e883c4b679f86ef7963721deda
Viince64 <vincentcrpg10@gmail.com>
2020-11-10 16:27:40 +0100

    menufinal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css'
 import Home from 'pages/Home';
+import { Route, Switch } from 'react-router-dom';
+import About from './pages/About';
 
 function App() {
   return (
-    <Home />
+    <Switch>
+      <Route path="/about">
+        <About/>
+      </Route>
+      <Route path="/">
+        <Home/>
+      </Route>
+    </Switch>
   );
 }
 
