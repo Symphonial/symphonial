d84a66e75724afd80b2e9aaeee78d3690544bc59
Viince64 <vincentcrpg10@gmail.com>
2020-11-11 12:58:53 +0100

    footer

--- src/pages/History/index.js
+++ src/pages/History/index.js
@@ -4,7 +4,7 @@ import Menu from 'components/Menu'
 
 const History = () => {
     return (
-        <div>
+        <div className={styles.History}>
             <Menu/>
             <h1 className={styles.classictitle}>Histoire du mois</h1>
         </div>
