5a8721f2f98b6f4044eba8f1ff36bf4001af1ccf
Lucas-Mar <lucas.marsalle@mmibordeaux.com>
2020-11-13 11:37:56 +0100

    Netlify

--- src/pages/MostListened/index.js
+++ src/pages/MostListened/index.js
@@ -1,21 +1,17 @@
 import React from 'react'
 import styles from './MostListened.module.css'
-import Menu from 'components/Menu'
+//import Menu from 'components/Menu'
 import Page from '../../components/Page'
 
 const MostListened = () => {
     return (
 <Page>        
         <div className={styles.mostlistened}>
-<<<<<<< HEAD
-            <h1 className={styles.classictitle}>Les plus écoutés</h1>
-            
-  
-=======
+
             <h1 className={styles.titre}> Les plus écoutés</h1>
             <h2 className={styles.mois}> Novembre </h2>
          
->>>>>>> 4161598caec2cf5b321fbd7a92d374d746d194b4
+
         </div>
 </Page>
     )
